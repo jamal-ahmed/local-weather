db7bdf6697daf4d42778c5e280bb0b75fb819886
sheikh.ahmed@atombank.co.uk <sheikh.ahmed@atombank.co.uk>
2021-04-24 07:19:30 +0100

    initial project setup

---
+++ LocalWeatherApp/LocalWeatherApp/LocalWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalWeatherAppApp.swift
+//  LocalWeatherApp
+//
+//  Created by Sheikh Ahmed on 24/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct LocalWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
